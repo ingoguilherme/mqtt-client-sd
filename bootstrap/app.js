412bdd2e8e0789e3c54b507a04ce28b31cc98d41
ingoguilherme <ingo_eyng_1992@hotmail.com>
2018-09-15 15:53:12 -0300

    clena code

--- bootstrap/app.js
+++ bootstrap/app.js
@@ -7,32 +7,12 @@ var app = express();
 
 app.use(logger('dev'));
 
+var client  = mqtt.connect('mqtt://localhost')
+
 consign()
 	.include('models')
 	.then('controllers')
 	.then('routes')
-	.into(app);
+	.into(app, client);
 
 module.exports = app;
-
-var port = process.env.PORT || 3001;
-
-app.listen(port, function () {
-    console.log('Servidor rodando em http://localhost:%s', port);
-});
-
-var client  = mqtt.connect('mqtt://localhost')
- 
-client.on('connect', function () {
-  client.subscribe('presence', function (err) {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt')
-    }
-  })
-})
- 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
-module.exports = function (app) {
-	app.get('/', app.controllers.tasks.index);
-	app.post('/', app.controllers.tasks.store);
-	app.post('/task/:id', app.controllers.tasks.update);
-	app.get('/task/:id', app.controllers.tasks.destroy);
+module.exports = function (app, client) {
+	client.on('connect', function () {
+		//TODO subscribe em todos as pessoas que ele segue
+		client.subscribe('presence', function (err) {
+			if (!err) {
+			  client.publish('presence', 'Hello mqtt')
+			}
+		})
+	})
+
+	client.on('message', function (topic, message) {
+		// message is Buffer
+		console.log(message.toString())
+		client.end()
+	})
 }
